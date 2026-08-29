d900fd6189ebf51bd63ebe536e0effa178940a73
Filippo <filipposorze@hotmail.it>
2026-08-29 18:28:37 +0200

    Creazione cards, creazione bottoni

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <header class="hub-header">
-    <h1>Titolo</h1>
+    <h1 class="titolo">Titolo</h1>
   </header>
 
   <main class="enigma-grid">
-    <button class="enigma-card" data-enigma="1">Enigma 1</button>
-    <button class="enigma-card" data-enigma="2">Enigma 2</button>
-    <button class="enigma-card" data-enigma="3">Enigma 3</button>
-    <button class="enigma-card" data-enigma="4">Enigma 4</button>
-    <button class="enigma-card" data-enigma="5">Enigma 5</button>
-    <button class="enigma-card" data-enigma="6">Enigma 6</button>
+    <div class="enigma-card" data-enigma="1"><div class="nomeEnigma">a</div><button class="bottone">Enigma 1</button></div>
+    <div class="enigma-card" data-enigma="2"><div class="nomeEnigma">a</div><button class="bottone">Enigma 2</button></div>
+    <div class="enigma-card" data-enigma="3"><div class="nomeEnigma">a</div><button class="bottone">Enigma 3</button></div>
+    <div class="enigma-card" data-enigma="4"><div class="nomeEnigma">a</div><button class="bottone">Enigma 4</button></div>
+    <div class="enigma-card" data-enigma="5"><div class="nomeEnigma">a</div><button class="bottone">Enigma 5</button></div>
+    <div class="enigma-card" data-enigma="6"><div class="nomeEnigma">a</div><button class="bottone">Enigma 6</button></div>
   </main>
 `
