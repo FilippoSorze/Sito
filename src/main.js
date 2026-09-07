3d59ed463a7f7074cad2e0d992c6fcd5d16c4311
Filippo <filipposorze@hotmail.it>
2026-09-07 22:54:15 +0200

    aggiunta piattaforma e movimento wasd personaggio

--- src/main.js
+++ src/main.js
@@ -21,8 +21,41 @@ document.querySelector('#app').innerHTML = `
 mostraHub()
 function mostraEnigma1() {
   document.querySelector('#app').innerHTML = `
-    <canvas id="gioco" width="600" height="300"></canvas>
+    <canvas id="gioco" width="2000" height="300"></canvas>
   `
+  const tasti = {
+    destra: false,
+    sinistra: false,
+    sotto: false,
+    sopra: false
+  }
+
+   const piattaforma = {
+    x: 300,
+    y: 200,
+    larghezza: 100,
+    altezza: 20
+  }
+
+  document.addEventListener('keydown', (event) =>{
+    if(event.code === 'KeyD') tasti.destra = true
+    if(event.code === 'KeyA') tasti.sinistra = true
+    if(event.code === 'KeyS') tasti.sotto = true
+    if(event.code === 'KeyW') tasti.sopra = true
+
+  })
+
+  
+  document.addEventListener('keyup', (event) =>{
+    if(event.code === 'KeyD') tasti.destra = false
+    if(event.code === 'KeyA') tasti.sinistra = false
+    if(event.code === 'KeyS') tasti.sotto = false
+    if(event.code === 'KeyW') tasti.sopra = false
+
+    console.log(event.code)
+  })
+
+
 
   const canvas = document.querySelector('#gioco')
   const ctx = canvas.getContext('2d')
@@ -30,12 +63,13 @@ function mostraEnigma1() {
   const terra = 250 // altezza personaggio quando è a terra
   const gravità = 0.5
 
+  let personaggioX = 50
   let personaggioY = terra
   let velocitàY = 0
 
   document.addEventListener('keydown', (event) => {
     if (event.code === 'Space' && personaggioY === terra) {
-      velocitàY = -10 //impulso verso l'alto
+      velocitàY = -13 //impulso verso l'alto
     }
   })
 
@@ -50,13 +84,29 @@ function mostraEnigma1() {
     velocitàY = 0
   }
 
+
+
+  if (tasti.destra) personaggioX = personaggioX + 3
+  if (tasti.sinistra) personaggioX = personaggioX - 3
+  if (tasti.sotto) velocitàY = velocitàY + 3
+  // if (tasti.sopra) velocitàY = velocitàY - 3
+
+
+
   
   
   
   ctx.fillStyle = 'red'
-  ctx.fillRect(50, personaggioY, 40, 40)
+  ctx.fillRect(personaggioX, personaggioY, 40, 40)
+ctx.fillStyle = 'blue'
+ctx.fillRect(piattaforma.x, piattaforma.y, piattaforma.larghezza, piattaforma.altezza)
+
+
+  
+if(velocitàY >= 0)
   
   requestAnimationFrame(loop) // chiama la funzione loop per 60 volte al secondo
+ 
 }
 loop()
 }
